1dd332d9a282184393f5d433d39731721579c4c0
Egor Yurtaev <yurtaev.egor@gmail.com>
2014-09-25 19:19:26 +0700

    added doctest for omaha.utils.get_sec_since_midnight

--- omaha/utils.py
+++ omaha/utils.py
@@ -4,6 +4,13 @@ __all__ = ['get_sec_since_midnight']
 
 
 def get_sec_since_midnight(date):
+    """
+    Return seconds since midnight
+
+    >>> from datetime import datetime
+    >>> get_sec_since_midnight(datetime(year=2014, month=1, day=1, second=42))
+    42
+    """
     midnight = date.replace(hour=0, minute=0, second=0, microsecond=0)
     delta = date - midnight
     return delta.seconds
