8eae4ccd01be0ce53478e3b0296cdbf9f8e74a17
Egor Yurtaev <yurtaev.egor@gmail.com>
2015-02-26 14:26:30 +0600

    Increase max length of symbol's debug ID

---
+++ omaha_server/crash/migrations/0014_auto_20150226_0824.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crash', '0013_auto_20150217_0927'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='symbols',
+            name='debug_id',
+            field=models.CharField(db_index=True, max_length=255, null=True, verbose_name=b'Debug ID', blank=True),
+            preserve_default=True,
+        ),
+    ]
