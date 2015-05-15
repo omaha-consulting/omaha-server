49ca0d43996048f5d1bd039761feca5500f3282f
Egor Yurtaev <yurtaev.egor@gmail.com>
2015-05-15 16:07:14 +0600

    Upgrade deps

--- omaha_server/omaha_server/settings.py
+++ omaha_server/omaha_server/settings.py
@@ -143,23 +143,23 @@ REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 
 CACHES = {
     'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': '{REDIS_HOST}:{REDIS_PORT}:{REDIS_DB}'.format(
             REDIS_PORT=REDIS_PORT,
             REDIS_HOST=REDIS_HOST,
             REDIS_DB=os.environ.get('REDIS_DB', 1)),
         'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     },
     'statistics': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': '{REDIS_HOST}:{REDIS_PORT}:{REDIS_DB}'.format(
             REDIS_PORT=os.environ.get('REDIS_STAT_PORT', REDIS_PORT),
             REDIS_HOST=os.environ.get('REDIS_STAT_HOST', REDIS_HOST),
             REDIS_DB=os.environ.get('REDIS_STAT_DB', 15)),
         'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
