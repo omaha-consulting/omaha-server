ac4b22b9eb643fb9448387e4ac8dd2652e40c7eb
Egor Yurtaev <yurtaev.egor@gmail.com>
2015-05-15 15:09:12 +0600

    Bump version: 0.0.7 → 0.0.8

--- omaha_server/omaha_server/settings.py
+++ omaha_server/omaha_server/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_CONTEXT_PROCESSORS = TCP + (
     'absolute.context_processors.absolute',
 )
 
-APP_VERSION = "0.0.7"
+APP_VERSION = "0.0.8"
 
 SUIT_CONFIG = {
     'ADMIN_NAME': 'Omaha Server [{}]'.format(APP_VERSION),
