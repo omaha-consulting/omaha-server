5364940f57ec71fd94022d7af306f5843bac1870
Egor Yurtaev <yurtaev@iMac-crystalnix.local>
2015-09-03 17:38:20 +0600

    testing travis-ci with postgres

---
+++ omaha_server/omaha_server/settings_test_postgres.py
@@ -0,0 +1,78 @@
+# coding: utf8
+
+import os
+
+os.environ.setdefault('DB_PUBLIC_USER', 'test_public_user')
+os.environ.setdefault('DB_PUBLIC_PASSWORD', 'test_public_password')
+
+os.environ.setdefault('OMAHA_SERVER_PRIVATE', 'True')
+
+from .settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'CONN_MAX_AGE': 60,
+        'TEST': {
+            'USER': os.environ.get('DB_PUBLIC_USER', 'postgres'),
+            'PASSWORD': os.environ.get('DB_PUBLIC_PASSWORD', ''),
+        }
+    }
+}
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+INSTALLED_APPS += (
+    'django_nose',
+)
+
+# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_RUNNER = 'omaha_server.runner.CustomNoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=omaha_server,omaha,crash,feedback,sparkle,healthcheck',
+    '--cover-inclusive',
+    '--nologcapture',
+]
+
+# Tricks to speed up Django tests
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+BROKER_BACKEND = 'memory'
+
+CACHES['default'] = {
+    'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+}
+
+CACHES['statistics'] = {
+    'BACKEND': 'django_redis.cache.RedisCache',
+    'LOCATION': '{REDIS_HOST}:{REDIS_PORT}:{REDIS_DB}'.format(
+        REDIS_PORT=os.environ.get('REDIS_STAT_PORT', REDIS_PORT),
+        REDIS_HOST=os.environ.get('REDIS_STAT_HOST', REDIS_HOST),
+        REDIS_DB=os.environ.get('REDIS_STAT_DB', 13)),
+    'OPTIONS': {
+        'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+    }
+}
+
+
+OMAHA_UID_KEY_PREFIX = 'test:uid'
+
+CRASH_SYMBOLS_PATH = os.path.join(BASE_DIR, 'crash', 'tests', 'testdata', 'symbols')
+CRASH_S3_MOUNT_PATH = os.path.join(BASE_DIR, 'crash', 'tests', 'testdata')
+
+RAVEN_DSN_STACKTRACE = 'http://c5dc6f5ab74b4ab8a567f545b00cb138:c57ee00766cf497da102b7a83d731840@127.0.0.1/1'
