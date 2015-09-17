74914f18b3c34a40e9fb202434b0a361790b86bf
Andrey Mekin <amekin@crystalnix.com>
2015-09-17 15:35:07 +0600

    Add GenericIPAddressField migration for feedback

---
+++ omaha_server/feedback/migrations/0004_auto_20150917_0932.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('feedback', '0003_auto_20150909_0755'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='feedback',
+            name='ip',
+            field=models.GenericIPAddressField(null=True, blank=True),
+        ),
+    ]
