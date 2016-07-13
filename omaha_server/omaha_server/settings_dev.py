5cf6817ec01c4f6c87dac31c5a10c3bdaa84c977
Andrey Mekin <amekin@crystalnix.com>
2016-07-13 15:11:29 +0600

    Remove HttpLog

--- omaha_server/omaha_server/settings_dev.py
+++ omaha_server/omaha_server/settings_dev.py
@@ -2,18 +2,3 @@
 
 from .settings_prod import *
 
-
-INSTALLED_APPS += (
-    'httplog',
-)
-
-MIDDLEWARE_CLASSES += (
-    'httplog.middleware.RequestResponseLoggingMiddleware',
-)
-
-HTTPLOG_URLNAMES = ['update', 'sparkle_appcast', 'crash']
-HTTPLOG_APPS = ['omaha', 'crash', 'sparkle']
-
-SUIT_CONFIG['MENU'] = SUIT_CONFIG['MENU'] + (
-    {'app': 'httplog', 'label': 'httplog', 'icon': 'icon-search'},
-)
