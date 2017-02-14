891fb37f9a96110e05d8293c9be6f3fd3b701ef9
Andrey Mekin <amekin@crystalnix.com>
2017-02-14 19:43:12 +0600

    Fix download page
    The latest versions on the download page shouldn't be disabled.

--- omaha_server/downloads/tests/test_api.py
+++ omaha_server/downloads/tests/test_api.py
@@ -3,7 +3,7 @@ import json
 from rest_framework.test import APITestCase
 from django.db import connections
 
-from omaha.factories import VersionFactory, ApplicationFactory, ChannelFactory
+from omaha.factories import VersionFactory, ApplicationFactory, ChannelFactory, PlatformFactory
 from sparkle.factories import SparkleVersionFactory
 
 
@@ -12,12 +12,15 @@ class DownloadsTest(APITestCase):
 
     def setUp(self):
         self.app = ApplicationFactory(name='TestApp')
+        self.platform = PlatformFactory(name='win')
         self.channel = ChannelFactory(name='alpha')
         self.stable_channel = ChannelFactory(name='stable')
-        self.win_v1 = VersionFactory(version='10.0.0.0', app=self.app, channel=self.channel)
-        self.win_v2 = VersionFactory(version='42.0.1.0', app=self.app, channel=self.channel)
-        self.win_stable_v = VersionFactory(version='23.0.0.0', app=self.app, channel=self.stable_channel)
-        self.win_disabled_v = VersionFactory(version='55.0.2.0', app=self.app, channel=self.channel, is_enabled=False)
+        self.win_v1 = VersionFactory(version='10.0.0.0', app=self.app, channel=self.channel, platform=self.platform)
+        self.win_v2 = VersionFactory(version='42.0.1.0', app=self.app, channel=self.channel, platform=self.platform)
+        self.win_stable_v = VersionFactory(version='23.0.0.0', app=self.app,
+                                           channel=self.stable_channel, platform=self.platform)
+        self.win_disabled_v = VersionFactory(version='55.0.2.0', app=self.app, channel=self.channel,
+                                             platform=self.platform, is_enabled=False)
 
         self.mac_v1 = SparkleVersionFactory(short_version='10.0.0.0', version='0.0',
                                             app=self.app, channel=self.channel)
@@ -58,3 +61,17 @@ class DownloadsTest(APITestCase):
         response = self.client.get('/api/downloads', format='json')
 
         self.assertEqual(json.loads(json.dumps(response.data)), self.exp_res)
+
+    def test_latest_mac_version(self):
+        if connections['default'].settings_dict['ENGINE'] != 'django.db.backends.postgresql_psycopg2':
+            self.skipTest('Database should be postgreSQL')
+        response = self.client.get('/downloads/latest/sparkle/TestApp/alpha/')
+
+        self.assertRedirects(response, self.mac_v2.file.url, fetch_redirect_response=False)
+
+    def test_latest_win_version(self):
+        if connections['default'].settings_dict['ENGINE'] != 'django.db.backends.postgresql_psycopg2':
+            self.skipTest('Database should be postgreSQL')
+        response = self.client.get('/downloads/latest/omaha/TestApp/win/alpha/')
+
+        self.assertRedirects(response, self.win_v2.file.url, fetch_redirect_response=False)
