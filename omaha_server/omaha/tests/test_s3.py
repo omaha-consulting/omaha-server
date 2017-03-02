0f27149aba20c1820067e4ea864929ec1d5beec3
Andrey Mekin <amekin@crystalnix.com>
2017-03-02 19:43:05 +0600

    Disable replacing white spaces in the symbol's filename

--- omaha_server/omaha/tests/test_s3.py
+++ omaha_server/omaha/tests/test_s3.py
@@ -13,8 +13,9 @@ from crash.models import Crash, Symbols
 from feedback.models import Feedback
 from omaha.models import Version
 from sparkle.models import SparkleVersion
-
+from omaha_server.utils import storage_with_spaces_instance
 from omaha.limitation import bulk_delete
+
 
 class BaseS3Test(object):
     model = None
@@ -92,6 +93,10 @@ class SymbolsS3Test(BaseS3Test, TestCase):
     model = Symbols
     factory = SymbolsFactory
     file_fields = ['file']
+
+    def setUp(self):
+        storage_with_spaces_instance._setup()
+
 
 
 @override_settings(DEFAULT_FILE_STORAGE='storages.backends.s3boto.S3BotoStorage')
