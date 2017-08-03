aa2239a7270c48789bd496134d69d35e97d50041
Egor Yurtaev <yurtaev.egor@gmail.com>
2017-08-03 18:03:50 +0600

    Add google-cloud

--- omaha_server/omaha_server/settings_google.py
+++ omaha_server/omaha_server/settings_google.py
@@ -1,5 +1,14 @@
 from .settings_prod import *
 
+DEBUG = True
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+
+
+STATIC_URL = '/static/'
+
+
+GS_BUCKET_NAME = 'omaha-server-dev'
+GS_PROJECT_ID = 'remarkable-nih-updateserver'
+GS_AUTO_CREATE_ACL = 'publicRead'
