accf468922165dd3028f417500000629bcddf212
jhorjus <jan.horjus@viasat.com>
2017-11-17 10:37:05 -0500

    TCPLogstashHandler version=1 changes.

--- omaha_server/crash/senders.py
+++ omaha_server/crash/senders.py
@@ -11,7 +11,7 @@ class BaseSender(object):
     name = None
     client = None
 
-    def send(self, message, extra={}, tags={}, data={}, crash_obj=None):
+    def send(self, message, extra={}, tags={}, sentry_data={}, crash_obj=None):
         pass
 
 
@@ -25,13 +25,13 @@ class SentrySender(BaseSender):
             release=getattr(settings, 'APP_VERSION', None)
         )
 
-    def send(self, message, extra={}, tags={}, data={}, crash_obj=None):
+    def send(self, message, extra={}, tags={}, sentry_data={}, crash_obj=None):
         event_id = self.client.capture(
             'raven.events.Message',
             message=message,
             extra=extra,
             tags=tags,
-            data=data
+            data=sentry_data
         )
         signature("tasks.get_sentry_link", args=(crash_obj.pk, event_id)).apply_async(queue='private', countdown=1)
 
@@ -44,16 +44,20 @@ class ELKSender(BaseSender):
         host = getattr(settings, 'LOGSTASH_HOST', None)
         port = getattr(settings, 'LOGSTASH_PORT', None)
         if host and port:
-            self.handler = TCPLogstashHandler(host, int(port))
+            self.handler = TCPLogstashHandler(host, int(port), version=1)
         else:
             logging.error("Logstash settings are not configured")
 
-    def send(self, message, extra={}, tags={}, data={}, crash_obj=None):
+    def send(self, message, extra={}, tags={}, sentry_data={}, crash_obj=None):
         if self.handler:
             logger = self._prepare_logger()
             extra.update(tags)
-            extra.update(data)
-            logger.info(message, extra=extra)
+            # We don't want "sentry.interfaces" as part of a field name.
+            extra['exception'] = sentry_data['sentry.interfaces.Exception']
+            # The "message" is actually a crash signature, not appropriate for the ELK "message" field.
+            extra['signature'] = message
+            # Send message with logger.
+            logger.info("Sparrow Crashes", extra=extra)
         else:
             logging.error("Logstash settings are not configured")
 
